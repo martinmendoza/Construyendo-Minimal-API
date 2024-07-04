81db42a2e5fa6553189565871e26971370ec7f80
MMendoza <martinmc893@gmail.com>
2024-07-04 13:37:40 -0600

    creando mi primer web API

---
+++ MinimalAPIPeliculas/MinimalAPIPeliculas/Program.cs
@@ -0,0 +1,34 @@
+﻿using MinimalAPIPeliculas.Entidades;
+
+var builder = WebApplication.CreateBuilder(args);
+//Inicio de areas de los servicios
+
+//Fin de areas de los servicios
+
+var app = builder.Build();
+
+//Inicio de  areas de middlewares
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/generos", () => {
+    var generos = new List<Genero>
+    {
+        new Genero
+        {
+            Id = 1,
+            Nombre = "Acción"
+
+        },
+        new Genero
+        {
+            Id = 1,
+            Nombre = "Comedia"
+
+        },
+    };  
+    return generos;
+
+});
+//Fin de areas de middlewares
+app.Run();
